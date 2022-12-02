9c1c949d24e37a427b55a5a7b15672ec57ebaf6a
MuhammadRifan <muhammadrifan153@gmail.com>
2022-12-02 23:24:51 +0700

    add store in both view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,14 +5,32 @@ export default {
 </script>
 <script setup lang="ts">
 import SearchField from "@/components/input_field/SearchField.vue";
+import { useListStore, type NoteStore } from "@/store/ListStore";
 import { ePage } from "@/util/enum";
 import Util from "@/util/utility";
+import { ref } from "vue";
 import { useRouter } from "vue-router";
 
 const router = useRouter();
+const listStore = useListStore();
 
-const go = () => {
+const limitNote = (notes: NoteStore[]) => {
+  const limit = ref(3);
+  const temp = [];
+
+  for (let i = 0; i < notes.length; i++) {
+    if (i < limit.value) {
+      temp.push(notes[i]);
+    } else continue;
+  }
+
+  return temp;
+};
+
+const goNote = (id?: number) => {
   Util.goPage(ePage.eNote, router);
+  if (id === undefined) listStore.idActive = listStore.data.length;
+  else listStore.idActive = id;
 };
 </script>
 
@@ -21,21 +39,68 @@ const go = () => {
     <SearchField str-placeholder="Search your notes" />
   </div>
   <div class="h-[76px]" />
-  <!-- <div class="border rounded-xl border-slate-100 m-[10px] p-[10px]">
-    Content
-  </div> -->
-  <div
+  <section v-if="listStore.data.length > 0">
+    <div
+      v-if="listStore.countPinned > 0"
+      id="pinned"
+      class="pt-[10px] pb-[15px]"
+    >
+      <div class="text-sm mx-[15px]">Pinned</div>
+      <div
+        v-for="list in listStore.data.filter((it) => it.pin == true)"
+        :key="list.id"
+        class="border rounded-2xl border-slate-100 mx-[15px] mt-[10px] p-[10px] cursor-pointer"
+        @click="goNote(list.id)"
+      >
+        <div class="font-semibold">{{ list.title }}</div>
+        <ul v-if="list.note.length > 0" class="list-disc py-[5px] px-[15px]">
+          <li v-for="note in limitNote(list.note)" :key="note.id">
+            <div class="truncate">
+              {{ note.note }}
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div
+      v-if="listStore.countOther > 0"
+      id="other"
+      :class="listStore.countPinned > 0 ? 'pt-[10px]' : ''"
+    >
+      <div v-if="listStore.countPinned > 0" class="text-sm mx-[15px]">
+        Other
+      </div>
+      <div
+        v-for="list in listStore.data.filter((it) => it.pin == false)"
+        :key="list.id"
+        class="border rounded-2xl border-slate-100 mx-[15px] mt-[10px] p-[10px] cursor-pointer"
+        @click="goNote(list.id)"
+      >
+        <div class="font-semibold">{{ list.title }}</div>
+        <ul v-if="list.note.length > 0" class="list-disc py-[5px] px-[15px]">
+          <li v-for="note in limitNote(list.note)" :key="note.id">
+            <div class="truncate">
+              {{ note.note }}
+            </div>
+          </li>
+          <div v-if="list.note.length > 3">...</div>
+        </ul>
+      </div>
+    </div>
+  </section>
+  <section
+    v-else
     class="flex flex-col w-full items-center text-center gap-y-[10px] my-[100px] opacity-30"
   >
     <span class="text-5xl material-symbols-outlined">lightbulb</span>
     <span class="text-lg">List you add appear here</span>
-  </div>
+  </section>
   <div
     class="fixed bottom-0 w-full max-w-sm p-[25px] flex items-center justify-end"
   >
     <span
       class="text-6xl cursor-pointer opacity-70 material-symbols-outlined"
-      @click="go()"
+      @click="goNote()"
     >
       add_circle
     </span>
--- src/views/NoteView.vue
+++ src/views/NoteView.vue
@@ -5,36 +5,165 @@ export default {
 </script>
 <script setup lang="ts">
 import TextField from "@/components/input_field/TextField.vue";
-import Util from "@/util/Utility";
+import NoteField from "@/components/input_field/NoteField.vue";
+import { useListStore } from "@/store/ListStore";
+import { ePage } from "@/util/enum";
+import Util from "@/util/utility";
+import { computed, onMounted, ref, type ComputedRef } from "vue";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+const listStore = useListStore();
+
+const goBack = () => Util.goPage(ePage.eBack, router);
+
+const pin = ref(false);
+const pinned = () => {
+  listStore.pin();
+  pin.value = listStore.getList.pin;
+};
+
+const titleElement: ComputedRef<HTMLElement | null> = computed(() =>
+  document.getElementById("titleElement")
+);
+const titleField = ref<InstanceType<typeof TextField> | null>(null);
+
+const title = ref("");
+const saveTitle = (submit: boolean) => {
+  if (submit) titleElement.value?.blur();
+  else if (title.value.length > 0) listStore.title(title.value);
+};
+
+const noteNewElement: ComputedRef<HTMLElement | null> = computed(() =>
+  document.getElementById("noteNewElement")
+);
+const noteNewField = ref<InstanceType<typeof TextField> | null>(null);
+
+const isFocus = ref(false);
+const onClose = ref(false);
+
+const isCreateNew = ref(false);
+const newNote = ref("");
+
+const createNewNote = () => {
+  isCreateNew.value = true;
+  noteNewElement.value?.focus();
+};
+
+const saveNewNote = (isSave: boolean) => {
+  if (isSave) {
+    if (isFocus.value && newNote.value.length > 0) {
+      isCreateNew.value = false;
+      listStore.addNote(newNote.value);
+      newNote.value = "";
+    }
+    unFocus();
+    isFocus.value = false;
+  } else {
+    isFocus.value = true;
+  }
+};
+
+const clearNewNote = () => {
+  newNote.value = "";
+  isFocus.value = false;
+  isCreateNew.value = false;
+  unFocus();
+};
+
+const unFocus = () => {
+  noteNewElement.value?.blur();
+  noteNewField.value?.updateModelValue("");
+};
+
+onMounted(() => {
+  const data = listStore.getList;
+  if (data != undefined) {
+    if (data.title != "") {
+      title.value = data.title;
+      titleField.value?.updateModelValue(title.value);
+    }
+    pin.value = data.pin;
+  }
+});
 </script>
 <template>
-  <div
-    class="fixed flex items-center p-[15px] w-full max-w-sm border-b gap-x-[10px]"
-  >
+  <div class="fixed flex items-center p-[15px] w-full max-w-sm gap-x-[10px]">
     <span
       class="cursor-pointer material-symbols-outlined text-slate-200"
-      @click="Util.goBack()"
+      @click="goBack()"
     >
       arrow_back_ios_new
     </span>
+    <TextField
+      v-model="title"
+      ref="titleField"
+      str-class="grow"
+      str-id="titleElement"
+      str-placeholder="Title"
+      str-class-input="text-center overflow-hidden"
+      @focusout="saveTitle(false)"
+      @keydown.enter="saveTitle(true)"
+      :i-max-length="25"
+      :b-border="false"
+      :b-simple="true"
+    />
     <span
-      class="overflow-hidden text-center cursor-pointer opacity-80 grow whitespace-nowrap"
+      class="cursor-pointer material-symbols-outlined text-slate-200"
+      :class="pin ? 'filled' : ''"
+      @click="pinned()"
     >
-      AAAAAAAAAAAAAAAAAAAAAAAAAAA
+      push_pin
     </span>
-    <span class="material-symbols-outlined text-slate-200">push_pin</span>
   </div>
   <div class="h-[54px]" />
   <div id="lists">
     <ul class="list-disc list-inside py-[10px] px-[15px]">
-      <li>test</li>
-      <div class="flex flex-row">
-        <li></li>
-        <TextField :b-border="false" :b-simple="true" />
+      <div
+        v-if="
+          listStore.getList != undefined && listStore.getList.note.length > 0
+        "
+      >
+        <NoteField
+          v-for="note in listStore.getList.note"
+          :key="note.id"
+          :id="note.id"
+          :note="note.note"
+        />
+      </div>
+      <div v-if="isCreateNew" class="flex flex-row">
+        <li />
+        <div class="flex items-center grow gap-x-[10px]">
+          <TextField
+            v-model="newNote"
+            ref="noteNewField"
+            str-id="noteNewElement"
+            str-class="grow h-[24px]"
+            str-class-input="h-[24px]"
+            @focusin="saveNewNote(false)"
+            @focusout="onClose ? clearNewNote() : saveNewNote(true)"
+            @keydown.enter="saveNewNote(true)"
+            :b-simple="true"
+            :b-border="false"
+            :b-text-area="true"
+            :b-auto-height="true"
+            :i-max-length="100"
+          />
+        </div>
+        <span
+          class="cursor-pointer material-symbols-outlined"
+          :class="isFocus ? 'visible' : 'invisible'"
+          @mouseenter="onClose = true"
+          @mouseout="onClose = false"
+        >
+          close
+        </span>
       </div>
     </ul>
     <div
-      class="border border-slate-300 w-max pl-[15px] pr-[20px] py-[5px] rounded-full mx-auto mt-[10px] flex items-center gap-x-[5px]"
+      v-if="!isCreateNew"
+      class="border border-slate-300 w-max pl-[15px] pr-[20px] py-[5px] rounded-full mx-auto flex items-center gap-x-[5px] cursor-pointer"
+      @click="createNewNote()"
     >
       <span class="material-symbols-outlined">add</span>
       Add List
